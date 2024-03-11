67d66b238ace7de576125f4b5164d229321be270
Nik0-9 <filannino.nicola9@gmail.com>
2024-03-11 14:08:48 +0100

    init

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+/*Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo. */
+
+let mailList = [
+'usermail-0@mail.com',
+'usermail-1@mail.com',
+'usermail-2@mail.com',
+'usermail-3@mail.com'
+];
+
+let userMail = document.getElementById('mailValue');
+let result = document.querySelector('result');
+
+
+
